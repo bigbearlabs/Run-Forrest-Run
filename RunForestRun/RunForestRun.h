12bc6a5aa48d88dd75e4db961e52ddf55956425e
Andy Park <andy@bigbearlabs.com>
2016-11-06 20:57:17 +0100

    finish integrating.

---
+++ RunForestRun/RunForestRun.h
@@ -0,0 +1,19 @@
+//
+//  RunForestRun.h
+//  RunForestRun
+//
+//  Created by ilo on 04.11.16.
+//  Copyright © 2016 Michele Gruppioni. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RunForestRun.
+FOUNDATION_EXPORT double RunForestRunVersionNumber;
+
+//! Project version string for RunForestRun.
+FOUNDATION_EXPORT const unsigned char RunForestRunVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RunForestRun/PublicHeader.h>
+
+
